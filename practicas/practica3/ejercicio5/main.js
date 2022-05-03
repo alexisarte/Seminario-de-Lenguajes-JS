c154eca1b2ffc4c5e8c769d2cffd484824ef4349
Alexis Rodriguez <87901815+alexisarte@users.noreply.github.com>
2022-05-03 19:57:37 -0300

    Update main.js

--- practicas/practica3/ejercicio5/main.js
+++ practicas/practica3/ejercicio5/main.js
@@ -1,19 +1,10 @@
-function rickAndMorty() {
-    const URL = 'https://rickandmortyapi.com/api/episode';
-    rickAndMortyRecursive(URL);
-}
-
-function rickAndMortyRecursive(URL) {
-    if (URL != null) {
-        fetch(URL)
-            .then(response => response.json())
-            .then(response => {
-                response.results.forEach(element => showEpisodes(element.name, element.air_date, element.episode));
-                return response.info.next;
-            })
-            .then(response => rickAndMortyRecursive(response))
-            .catch(err => console.log(err));
-    }
+function fetchRickAndMorty() {
+    fetch('https://rickandmortyapi.com/api/episode')
+        .then(response => response.json())
+        .then(response => {
+            response.results.forEach(element => showEpisodes(element.name, element.air_date, element.episode));
+        })
+        .catch(err => console.log(err));
 }
 
 function showEpisodes(name, air_date, episode) {
@@ -25,33 +16,13 @@ function showEpisodes(name, air_date, episode) {
     unorderedList.appendChild(br);
 }
 
-function filterRecursive(URL) {
-    let unorderedList = document.getElementById('unorderedList');
-    if (URL != null && URL != 'https://rickandmortyapi.com/api/episode/?name=' && !URL.includes('page')) {
-        while (unorderedList.firstChild != null) {
-            unorderedList.removeChild(unorderedList.firstChild);
-        }
-    }
-    if (URL != null && URL != 'https://rickandmortyapi.com/api/episode/?name=') {
-        fetch(URL)
-            .then(response => response.json())
-            .then(response => {
-
-                response.results.forEach(element => showEpisodes(element.name, element.air_date, element.episode));
-                return response.info.next;
-            })
-            .then(response => filterRecursive(response))
-            .catch(function () { unorderedList.innerHTML = 'The text entered does not match any title' });
-    } else if (URL == 'https://rickandmortyapi.com/api/episode/?name=') {
-        unorderedList.innerHTML = 'The text entered does not match any title'
-    }
-}
-
 function filter() {
-    const VALUE = document.getElementById("filter").value;
-    const url = 'https://rickandmortyapi.com/api/episode/?name=' + VALUE;
-    filterRecursive(url);
+    let VALUE = document.querySelector("filter").value;
+    let url = 'https://rickandmortyapi.com/api/episode/?name=' + VALUE;
+    while (unorderedList.firstChild != null) {
+        unorderedList.removeChild(unorderedList.firstChild);
+    }
+    fetchRickAndMorty(url);
 }
 
-// const button = document.getElementById("button");
-// button.addEventListener('click', filter());
+window.addEventListener('load', fetchRickAndMorty);
