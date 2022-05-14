051d2610826282da7f81af4f845490723acc3a9b
Alexis Rodriguez <87901815+alexisarte@users.noreply.github.com>
2022-05-14 17:20:16 -0300

    lógica pokéapi

--- practicas/practica3/ejercicio7/main.js
+++ practicas/practica3/ejercicio7/main.js
@@ -1,8 +1,12 @@
-const pokemonContainer = document.querySelector(".pokemon-container");
+const apiUrl = "https://pokeapi.co/api/v2/";
+const pageUrl = apiUrl + 'pokemon/?limit=20&offset=';
 
-function fetchPokemon(id) {
-    const URL = 'https://pokeapi.co/api/v2/pokemon/' + id + '/';
-    fetch(URL)
+const pokemonList = document.querySelector(".pokemon-list");
+const pokemonData = document.querySelector(".pokemon-data");
+
+function fetchPokemon(name) {
+    const url = 'https://pokeapi.co/api/v2/pokemon/' + name;
+    fetch(url)
         .then((res) => res.json())
         .then((data) => {
             createPokemon(data)
@@ -11,31 +15,57 @@ function fetchPokemon(id) {
 
 }
 
-function fetchPokemons(number) {
-    for (let i = 1; i < number; i++) {
-        fetchPokemon(i);
+function fetchPokemons(url) {
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => data.results.forEach(element => showPokemonName(element)))
+        .catch(err => console.log(err));
+}
+
+function showPokemonName(pokemon) {
+    const name = document.createElement('li');
+    name.textContent = pokemon.name;
+    name.addEventListener('click', showPokemon);
+    pokemonList.appendChild(name);
+}
+
+let showPokemon = function () {
+    while (pokemonData.firstChild != null) {
+        pokemonData.removeChild(pokemonData.firstChild);
     }
+    fetchPokemon(this.textContent);
 }
 
 function createPokemon(pokemon) {
     const card = document.createElement('div');
-    card.classList.add('pokemon-block');
-
-    const spriteContainer = document.createElement('div');
-    spriteContainer.classList.add('img-container');
-
     const sprite = document.createElement('img');
     sprite.src = pokemon.sprites.front_default
-
-    spriteContainer.appendChild(sprite);
-
     const name = document.createElement('p');
     name.classList.add('name');
     name.textContent = pokemon.name;
-
-    card.appendChild(spriteContainer);
     card.appendChild(name);
-    pokemonContainer.appendChild(card);
+    card.appendChild(sprite);
+    pokemonData.appendChild(card);
 }
 
-fetchPokemons(10);
+function pages() {
+    let options = document.querySelector('.options');
+    for (let i = 0; i < 58; i++) {
+        let option = document.createElement('option');
+        option.value = i;
+        option.textContent = 'Page ' + (i + 1);
+        options.appendChild(option);
+    }
+}
+
+function pokemonsList() {
+    let options = document.querySelector('.options');
+    pokemonList.innerHTML = '';
+    let url = pageUrl + options.value * 20;
+    fetchPokemons(url);
+}
+
+pages();
+let options = document.querySelector('.options');
+window.addEventListener('load', fetchPokemons('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20'));
+options.addEventListener('change', pokemonsList);
