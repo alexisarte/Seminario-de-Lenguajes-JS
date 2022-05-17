595b4d547baea126811b11cb945702781eef5f7b
Alexis Rodriguez <87901815+alexisarte@users.noreply.github.com>
2022-05-17 19:12:23 -0300

    Update index.js

--- practicas/practica4/ejercicio2/index.js
+++ practicas/practica4/ejercicio2/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const EMAIL = example@gmail.com; 
+const USERNAME = pancracio;
+
 // Middlewares
 app.use(express.static('public'));
 
@@ -10,13 +13,18 @@ app.use(express.urlencoded({
 }));
 
 //Routes
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/exit', (req, res) => {
+    res.send('EXIT!');
 });
 
 app.post('/submit-form', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
+    if (username == USERNAME && password == PASSWORD) {
+        res.redirect('/exit');
+    } else {
+        res.redirect('/');
+    }
     console.log('Username: ' + username);
     console.log('Password: ' + password);
     res.end();
