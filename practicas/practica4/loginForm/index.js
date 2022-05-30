297e62052cf839b60a26ea931a041b35991f2b7b
alexisarte <alexisrodriguezarteaga@gmail.com>
2022-05-30 14:18:07 -0300

    ejercicios 1, 2 y 3

--- practicas/practica4/loginForm/index.js
+++ practicas/practica4/loginForm/index.js
@@ -2,25 +2,21 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-const USERNAME = 'admin@mail.com'; 
-const PASSWORD = '1234';
-
 // Middlewares
-app.use(express.static('public'));
+app.use(express.static('practicas\\practica4\\loginForm\\public'));
 
 app.use(express.urlencoded({
     extended: true
 }));
 
 //Routes
-app.get('/exit', (req, res) => {
-    res.send('EXIT!');
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
 app.post('/submit-form', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    ((username == USERNAME) && (password == PASSWORD)) ? res.redirect('/exit') : res.redirect('/'); 
     console.log('Username: ' + username);
     console.log('Password: ' + password);
     res.end();
